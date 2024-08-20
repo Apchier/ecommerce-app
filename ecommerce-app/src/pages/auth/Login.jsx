e8da7ad845040ab268fe6cf2135007226a21cb40
rafi andrea <rafiandrealesmana@gmail.com>
2024-08-20 21:17:08 +0700

    update resource and login page

--- ecommerce-app/src/pages/auth/Login.jsx
+++ ecommerce-app/src/pages/auth/Login.jsx
@@ -1,23 +1,29 @@
 import chairLogin from "../../../public/assets/images/loginchair.jpg"
 import ButtonLogin from "../../components/elements/ButtonLogin"
+import logo from "../../../public/assets/images/logo.png"
+import { FiArrowLeft } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
     <div className="flex w-full h-screen justify-center font-poppins">
       <div className="flex justify-center w-[1600px] min-h-screen">
-        <div className="flex items-center w-1/2 justify-center min-h-screen bg-gray-50 py-6 sm:py-12">
-          <div className="relative py-3 w-[450px] text-center bg-white shadow-xl p-10">
-            <div>
-              <img src="/assets/logo.png" alt="Logo" className="mx-auto h-12 w-auto" />
-              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                Get Started
+        <div className="relative flex items-center w-1/2 justify-center min-h-screen bg-gray-50 sm:py-12">
+          <Link to={"/"} className="absolute flex justify-center items-center top-5 left-5 w-[40px] aspect-square border-2 border-gray-600 rounded-full">
+            <FiArrowLeft size={25} />
+          </Link>
+          <div className="relative py-8 w-[450px] text-center bg-white shadow-lg p-10 rounded-xl">
+            <div className="relative">
+              <img src={logo} alt="Logo" className="mx-auto w-[125px] aspect-square" />
+              <h2 className="text-center text-3xl font-bold text-gray-700">
+                Welcome back
               </h2>
               <p className="mt-2 text-center text-sm text-gray-600">
-                Welcome to Filianta - Lets create your account
+                Please enter your details sign in
               </p>
             </div>
-            <form className="flex flex-col py-6 gap-4">
-              <div className="flex flex-col gap-4 rounded-md shadow-sm">
+            <form className="flex flex-col py-6 gap-2">
+              <div className="flex flex-col gap-4 rounded-md">
                 <div className="flex flex-col items-start gap-3">
                   <label htmlFor="email" className="text-gray-600">Email</label>
                   <input
@@ -29,7 +35,7 @@ export default function Login() {
                   />
                 </div>
                 <div className="flex flex-col items-start gap-3">
-                <label htmlFor="email" className="text-gray-600">Password</label>
+                  <label htmlFor="email" className="text-gray-600">Password</label>
                   <input
                     name="password"
                     type="password"
@@ -39,21 +45,21 @@ export default function Login() {
                   />
                 </div>
               </div>
-              <div className="flex items-center justify-between">
+              <div className="flex items-center justify-betwee">
                 <div className="text-sm">
                   <a href="#" className="font-medium text-green-600 hover:text-green-500">
                     Forgot your password?
                   </a>
                 </div>
               </div>
-              <div>
-                <ButtonLogin text={"Login"}/>
+              <div className="mt-4">
+                <ButtonLogin text={"Login"} />
               </div>
             </form>
             <div className="mt-4 text-center text-sm text-gray-600">
-              Already have an account?{' '}
+              Dont have an account?{' '}
               <a href="#" className="font-medium text-green-600 hover:text-green-500">
-                Log in
+                Create account
               </a>
             </div>
           </div>
