dd6c162d2d153de206653c9e3ebd0cd0452aa208
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2024-08-20 21:45:43 +0700

    fix dashbaord bug

--- ecommerce-app/src/pages/admin/dashboard/Dashboard.jsx
+++ ecommerce-app/src/pages/admin/dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import vector from "../../../../public/assets/images/Vector-Dashboard.png"
 import { FaAngleUp, FaAngleDown } from "react-icons/fa";
 import { Line } from "react-chartjs-2";
+import { Chart } from "chart.js/auto";
 
 export default function Dashboard() {
 
