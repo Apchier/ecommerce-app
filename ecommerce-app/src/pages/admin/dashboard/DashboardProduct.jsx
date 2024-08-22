489bac786aa2ebffbacc9a44aeb44941cd97d543
aldyfaisal80 <aldyfaisal80@gmail.com>
2024-08-23 00:37:52 +0700

    Message: Change table style
    modified:   ecommerce-app/src/pages/admin/dashboard/DashboardProduct.jsx

--- ecommerce-app/src/pages/admin/dashboard/DashboardProduct.jsx
+++ ecommerce-app/src/pages/admin/dashboard/DashboardProduct.jsx
@@ -59,16 +59,16 @@ export default function DashboardProduct() {
 
   const renderElements = () => {
     return products.map((product, index) => (
-      <tr key={index} className={`${index % 2 === 0 ? ' bg-[#eeeef1] ' : 'bg-white'}`}>
-        <td className="p-1">{(currentPage - 1) * productsPerPage + index + 1}</td>
-        <td className="p-1">{product.name}</td>
-        <td className="p-1">{product.category}</td>
-        <td className="p-1">{product.price}</td>
+      <tr key={index}>
+        <td className="py-4">{(currentPage - 1) * productsPerPage + index + 1}</td>
+        <td className="py-4">{product.name}</td>
+        <td className="py-4">{product.category}</td>
+        <td className="py-4">{product.price}</td>
         <td className="text-center p-1 font-semibold">
           <div className="flex justify-evenly items-center gap-2">
-            <button className="bg-gray-100 text-gray-600 shadow-md px-4 py-2 rounded-l hover:bg-gray-300 transition-all ease-in-out duration-300">Edit</button>
-            <Link to={`/dashboard/detail/${product.id}`} className="bg-gray-100 text-gray-600 shadow-md px-4 py-2 rounded-l hover:bg-gray-300 transition-all ease-in-out duration-300">View</Link>
-            <button className="bg-gray-100 text-gray-600 shadow-md px-4 py-2 rounded-l hover:bg-gray-300 transition-all ease-in-out duration-300" onClick={() => deleteProduct(product.id)}>Delete</button>
+            <button className="bg-gray-100 text-gray-600 shadow-md px-4 py-2 rounded-l hover:bg-gray-300 transition-all ease-in-out duration-300 w-[90px]">Edit</button>
+            <Link to={`/dashboard/detail/${product.id}`} className="bg-gray-100 text-gray-600 shadow-md px-4 py-2 rounded-l hover:bg-gray-300 transition-all ease-in-out duration-300 w-[90px]">View</Link>
+            <button className="bg-gray-100 text-gray-600 shadow-md px-4 py-2 rounded-l hover:bg-gray-300 transition-all ease-in-out duration-300 w-[90px]" onClick={() => deleteProduct(product.id)}>Delete</button>
           </div>
         </td>
 
@@ -89,32 +89,32 @@ export default function DashboardProduct() {
   };
 
   return (
-    <div className="flex w-[1600px] h-screen container justify-center font-poppins">
-      <div className="flex w-full justify-center rounded-xl shadow-lg bg-white">
-        <table className="w-full text-center">
-          <thead className="text-center text-gray-600">
-            <tr className="text-center border-b-2 border-gray-500">
+    <div className="flex w-[1600px] h-screen container justify-center font-poppins ">
+      <div className="flex w-full justify-center">
+        <table className="w-3/4 p-20 text-center rounded-xl shadow-lg bg-white divide-y-2 divide-gray-200">
+          <thead className="text-center text-text-gray bg-bg-gray">
+            <tr className="">
               <th className="p-4">
-                <p className="flex w-[250px] pl-[70px] justify-center gap-[30px] items-center cursor-pointer">No<PiCaretUpDown className="" /></p>
+                <p className="flex justify-evenly pl-8 items-center cursor-pointer">No<PiCaretUpDown className="" /></p>
               </th>
               <th className="p-4">
-                <p className="flex w-[250px] pl-[70px] justify-center gap-[30px] items-center cursor-pointer">Name<PiCaretUpDown className="" /></p>
+                <p className="flex justify-evenly pl-8 items-center cursor-pointer">Name<PiCaretUpDown className="" /></p>
               </th>
               <th className="p-4">
-                <p className="flex w-[250px] pl-[70px] justify-center gap-[30px] items-center cursor-pointer">Category<PiCaretUpDown className="" /></p>
+                <p className="flex justify-evenly pl-8 items-center cursor-pointer">Category<PiCaretUpDown className="" /></p>
               </th>
               <th className="p-4">
-                <p className="flex w-[250px] pl-[70px] justify-center gap-[30px] items-center cursor-pointer">Price<PiCaretUpDown className="" /></p>
+                <p className="flex justify-evenly pl-8 items-center cursor-pointer">Price<PiCaretUpDown className="" /></p>
               </th>
               <th className="p-4">
-                <p className="flex w-[250px] pl-[70px] justify-center gap-[30px] items-center cursor-pointer">Action</p>
+                <p className="flex justify-center items-center cursor-pointer">Action</p>
               </th>
             </tr>
           </thead>
-          <tbody className="text-center">
+          <tbody className="divide-y-2 divide-gray-200">
             {renderElements()}
-            <td className="w-full flex justify-center">
-              <div className="join ">
+            <td colSpan={5} className="pt-8 ">
+              <div className="join">
                 <button className="join-item btn" onClick={handlePreviousPage} disabled={currentPage === 1}>
                   «
                 </button>
