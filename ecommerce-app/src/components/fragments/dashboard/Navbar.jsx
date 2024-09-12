66bfb3dec8ed0741db2a19318116ac9db751198d
rafi andrea <rafiandrealesmana@gmail.com>
2024-09-12 12:57:57 +0700

    updating navbar

--- ecommerce-app/src/components/fragments/dashboard/Navbar.jsx
+++ ecommerce-app/src/components/fragments/dashboard/Navbar.jsx
@@ -1,23 +1,67 @@
+import { useState, useRef, useEffect } from "react";
+import { IoMenu } from "react-icons/io5";
 import NavLink from "../../elements/NavLink";
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const handleClickOutside = (event) => {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     return (
-        <nav className="sticky top-0 flex justify-around items-center text-xl font font-semibold w-[1200px] text-gray-600 h-[80px] border-2 border-gray-300 rounded-b-3xl bg-[#f7f7f8] shadow-md z-10">
-            <div className="flex justify-center items-center w-[200px]">
-                <NavLink text={"Home"} href={"/"} />
+        <nav className="sticky top-0 flex justify-between items-center text-xl font-semibold w-[1200px] text-gray-600 h-[80px] border-2 border-gray-300 rounded-b-3xl bg-[#f7f7f8] shadow-md z-10 container">
+            <div className="flex justify-start items-center w-full ml-14">
+                <div className="flex justify-start items-center w-auto mr-10">
+                    <NavLink text={"Dashboard"} href={"/dashboard"} />
+                </div>
+                <div className="flex justify-start items-center w-auto mr-10">
+                    <NavLink text={"Product List"} href={"/dashboard/product"} />
+                </div>
+                <div className="flex justify-start items-center w-auto mr-10">
+                    <NavLink text={"Create"} href={"/dashboard/create"} />
+                </div>
             </div>
-            <div className="flex justify-center items-center w-[200px] ">
-                <NavLink text={"All Products"} href={"/shopping-cart"} />
-            </div>
-            <div className="flex justify-center items-center w-[200px]">
-                <NavLink text={"Dashboard"} href={"/dashboard"} />
-            </div>
-            <div className="flex justify-center items-center w-[200px]">
-                <NavLink text={"Product List"} href={"/dashboard/product"} />
-            </div>
-            <div className="flex justify-center items-center w-[200px]">
-                <NavLink text={"Create"} href={"/dashboard/create"} />
+
+
+            <div className="flex justify-end items-center w-1/2">
+                <div className="relative flex justify-center items-center w-[200px] border-l-2 border-gray-300" ref={menuRef}>
+                    <button
+                        className="w-[50px] h-[50px] text-4xl flex justify-center items-center"
+                        onClick={toggleMenu}
+                    >
+                        <IoMenu />
+                    </button>
+
+                    {isOpen && (
+                        <div className="absolute top-[80px] right-0 w-[200px] bg-white shadow-lg border-2 border-gray-300 rounded-lg">
+                            <ul className="flex flex-col items-center space-y-2 p-4">
+                                <li>
+                                    <NavLink text={"Home"} href={"/"} />
+                                </li>
+                                <li>
+                                    <NavLink text={"Logout"} href={"/auth/logout"} />
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+                </div>
             </div>
         </nav>
-    )
+    );
 }
--- ecommerce-app/src/components/fragments/guest/Navbar.jsx
+++ ecommerce-app/src/components/fragments/guest/Navbar.jsx
@@ -1,23 +1,68 @@
+import { useState, useRef, useEffect } from "react";
+import { IoMenu } from "react-icons/io5";
 import NavLink from "../../elements/NavLink";
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const handleClickOutside = (event) => {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setIsOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside)
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+    }, []);
+
     return (
-        <nav className="sticky top-0 flex justify-around items-center text-xl font-semibold w-[1200px] text-gray-600 h-[80px] border-2 border-gray-300 rounded-b-3xl bg-[#f7f7f8] shadow-md z-10 container">
-            <div className="flex justify-center items-center w-[200px] ">
-                <NavLink text={"Home"} href={"/"} />
+        <nav className="sticky top-0 flex justify-between items-center text-xl font-semibold w-[1200px] text-gray-600 h-[80px] border-2 border-gray-300 rounded-b-3xl bg-[#f7f7f8] shadow-md z-10 container">
+            <div className="flex justify-start items-center w-full ml-14">
+                <div className="flex justify-start items-center w-auto mr-10">
+                    <NavLink text={"Home"} href={"/"} />
+                </div>
+                <div className="flex justify-start items-center w-auto mr-10">
+                    <NavLink text={"All Products"} href={"/shopping-cart"} />
+                </div>
             </div>
-            <div className="flex justify-center items-center w-[200px] ">
-                <NavLink text={"All Products"} href={"/shopping-cart"} />
-            </div>
-            <div className="flex justify-center items-center w-[200px] ">
-                <NavLink text={"Dashboard"} href={"/dashboard"} />
-            </div>
-            <div className="flex justify-center items-center w-[200px] ">
-                <NavLink text={"Login"} href={"/auth/login"} />
-            </div>
-            <div className="flex justify-center items-center w-[200px] ">
-                <NavLink text={"Register"} href={"/auth/register"} />
+
+
+
+            <div className="flex justify-end items-center w-1/2">
+                <div className="relative flex justify-center items-center w-[200px] border-l-2 border-gray-300" ref={menuRef}>
+                    <button
+                        className="w-[50px] h-[50px] text-4xl flex justify-center items-center"
+                        onClick={toggleMenu}
+                    >
+                        <IoMenu />
+                    </button>
+
+                    {isOpen && (
+                        <div className="absolute top-[80px] right-0 w-[200px] bg-white shadow-lg border-2 border-gray-300 rounded-lg">
+                            <ul className="flex flex-col items-center space-y-2 p-4">
+                                <li>
+                                    <NavLink text={"Dashboard"} href={"/dashboard"} />
+                                </li>
+                                <li>
+                                    <NavLink text={"Login"} href={"/auth/login"} />
+                                </li>
+                                <li>
+                                    <NavLink text={"Register"} href={"/auth/register"} />
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+                </div>
             </div>
         </nav>
-    )
+    );
 }
